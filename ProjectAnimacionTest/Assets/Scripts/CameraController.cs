fa3b9df0e117dfc327e2777494cb47d1c4ce259f
Astul Betizagasti <astul_95@hotmail.com>
2018-09-30 20:39:24 -0300

    parte final

---
+++ ProjectAnimacionTest/Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    public float smoothing = 10;
+
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+        transform.position = Vector3.Lerp(transform.position, player.transform.position + offset, smoothing * Time.deltaTime);
+    }
+}
---
+++ ProjectAnimacionTest/Assets/Scripts/Movimiento.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Audio;
+
+public class Movimiento : MonoBehaviour
+{
+
+    public Camera cam;
+
+    public NavMeshAgent agent;
+    public float attackRange;
+    public List<AudioClip> SwordSoundsAir;
+
+
+    Animator animator;
+    bool pegarAlLLegar = false;
+    AudioSource audioSource;
+    
+
+
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+        //agent.updateRotation = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("a"))
+        {
+            animator.SetTrigger("Saluda");
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+                pegarAlLLegar = false;
+            }
+        }
+        if (Input.GetMouseButton(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (Vector3.Distance(transform.position, hit.point) < attackRange)
+                {
+                    audioSource.clip = PickRandomSound(SwordSoundsAir);
+                    audioSource.Play();
+                    
+                    animator.SetTrigger("ataque");
+                    agent.destination = transform.position;
+                    pegarAlLLegar = false;
+                }
+                else
+                {
+                    agent.SetDestination(hit.point);
+                    pegarAlLLegar = true;
+                }
+            }
+        }
+        if (Vector3.Distance(transform.position, agent.destination) > 0.2)
+        {
+            animator.SetBool("caminando", true);
+        }
+        else
+        {
+            animator.SetBool("caminando", false);
+            if (agent.updatePosition && pegarAlLLegar)
+            {
+                audioSource.clip = PickRandomSound(SwordSoundsAir);
+                audioSource.Play();
+                animator.SetTrigger("ataque");
+                pegarAlLLegar = false;
+            }
+        }
+    }
+
+    AudioClip PickRandomSound(List<AudioClip> audioList)
+    {
+        return audioList[Random.Range(0, audioList.Count)];
+    }
+}
